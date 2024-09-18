d5b7608fc853788de86ba563e1b61373cdb3f114
kong <kong@92monkeys.com>
2024-09-18 13:24:01 +0900

    Add RenderObject

---
+++ Danburite/Render/RenderObject.cpp
@@ -0,0 +1,168 @@
+#include "RenderObject.h"
+
+namespace Render
+{
+	RenderObject::RenderObject()
+	{
+		__pMaterialPackMaterialChangeListener =
+			Infra::EventListener<MaterialPack const *, std::type_index, Material const *, Material const *>::bind(
+				&RenderObject::__onMaterialPackMaterialChanged, this,
+				std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
+
+		auto pMaterialPack{ std::make_unique<MaterialPack>() };
+		pMaterialPack->getMaterialChangeEvent() += __pMaterialPackMaterialChangeListener;
+
+		__materialPack2Index[pMaterialPack.get()] = 0U;
+		__materialPacks.emplace_back(std::move(pMaterialPack));
+	}
+
+	std::shared_ptr<Renderer const> const &RenderObject::getRenderer() const noexcept
+	{
+		return __pRenderer;
+	}
+
+	void RenderObject::setRenderer(
+		std::shared_ptr<Renderer const> const &pRenderer)
+	{
+		if (__pRenderer == pRenderer)
+			return;
+
+		auto const pPrevRenderer{ __pRenderer.get() };
+		__pRenderer = pRenderer;
+
+		__rendererChangeEvent.invoke(this, pPrevRenderer, pRenderer.get());
+		__validateDrawable();
+	}
+
+	std::shared_ptr<Mesh const> const &RenderObject::getMesh() const noexcept
+	{
+		return __pMesh;
+	}
+
+	void RenderObject::setMesh(
+		std::shared_ptr<Mesh const> const &pMesh)
+	{
+		if (__pMesh == pMesh)
+			return;
+
+		auto const pPrevMesh{ __pMesh.get() };
+		__pMesh = pMesh;
+
+		__meshChangeEvent.invoke(this, pPrevMesh, pMesh.get());
+		__validateDrawable();
+	}
+
+	std::shared_ptr<DrawParam const> const &RenderObject::getDrawParam() const noexcept
+	{
+		return __pDrawParam;
+	}
+
+	void RenderObject::setDrawParam(
+		std::shared_ptr<DrawParam const> const &pDrawParam)
+	{
+		if (__pDrawParam == pDrawParam)
+			return;
+
+		auto const pPrevDrawParam{ __pDrawParam.get() };
+		__pDrawParam = pDrawParam;
+
+		__drawParamChangeEvent.invoke(this, pPrevDrawParam, pDrawParam.get());
+		__validateDrawable();
+	}
+
+	uint32_t RenderObject::getInstanceCount() const noexcept
+	{
+		return static_cast<uint32_t>(__materialPacks.size());
+	}
+
+	void RenderObject::setInstanceCount(
+		uint32_t const count)
+	{
+		uint32_t const prevCount{ getInstanceCount() };
+		if (prevCount == count)
+			return;
+
+		std::vector<std::pair<uint32_t, std::shared_ptr<MaterialPack const>>> expiredPacks;
+
+		for (uint32_t expiredIt{ prevCount - 1U }; expiredIt >= count; --expiredIt)
+		{
+			auto &pExpiredPack{ __materialPacks[expiredIt] };
+			pExpiredPack->getMaterialChangeEvent() -= __pMaterialPackMaterialChangeListener;
+
+			__materialPack2Index.erase(pExpiredPack.get());
+			expiredPacks.emplace_back(expiredIt, std::move(pExpiredPack));
+		}
+
+		for (uint32_t instanceIt{ prevCount }; instanceIt < count; ++instanceIt)
+		{
+			auto pMaterialPack{ std::make_unique<MaterialPack>() };
+			pMaterialPack->getMaterialChangeEvent() += __pMaterialPackMaterialChangeListener;
+
+			__materialPack2Index[pMaterialPack.get()] = instanceIt;
+			__materialPacks.emplace_back(std::move(pMaterialPack));
+		}
+
+		for (auto const &[instanceIndex, pExpiredPack] : expiredPacks)
+		{
+			for (const auto pMaterial : *pExpiredPack)
+				__materialChangeEvent.invoke(this, instanceIndex, typeid(*pMaterial), pMaterial, nullptr);
+		}
+
+		__instanceCountChangeEvent.invoke(this, prevCount, count);
+
+		if (prevCount < count)
+			__validateDrawable();
+	}
+
+	void RenderObject::setVisible(
+		bool const visible)
+	{
+		if (__visible == visible)
+			return;
+
+		__visible = visible;
+		__validateDrawable();
+	}
+
+	void RenderObject::draw(
+		VK::CommandBuffer &cmdBuffer,
+		uint32_t baseId) const noexcept
+	{
+		__pDrawParam->draw(cmdBuffer, getInstanceCount(), baseId);
+	}
+
+	bool RenderObject::__resolveDrawable() const noexcept
+	{
+		if (!__pRenderer || !__pMesh || !__pDrawParam || !__visible)
+			return false;
+
+		for (auto const &pMaterialPack : __materialPacks)
+		{
+			if (!(__pRenderer->isValidMaterialPack(*pMaterialPack)))
+				return false;
+		}
+
+		return true;
+	}
+
+	void RenderObject::__validateDrawable()
+	{
+		bool const curDrawable{ __resolveDrawable() };
+		if (__drawable == curDrawable)
+			return;
+
+		__drawable = curDrawable;
+		__drawableChangeEvent.invoke(this, curDrawable);
+	}
+
+	void RenderObject::__onMaterialPackMaterialChanged(
+		MaterialPack const *pPack,
+		std::type_index const &type,
+		Material const *pPrev,
+		Material const *pCur)
+	{
+		uint32_t const instanceIndex{ __materialPack2Index.at(pPack) };
+		__materialChangeEvent.invoke(this, instanceIndex, type, pPrev, pCur);
+		__validateDrawable();
+	}
+}
--- Danburite/Render/RenderObject.h
+++ Danburite/Render/RenderObject.h
@@ -1,10 +1,174 @@
 #pragma once
 
+#include "Renderer.h"
+#include "Mesh.h"
+#include "DrawParam.h"
+
 namespace Render
 {
 	class RenderObject : public Infra::Unique
 	{
 	public:
 		RenderObject();
+
+		[[nodiscard]]
+		std::shared_ptr<Renderer const> const &getRenderer() const noexcept;
+		void setRenderer(
+			std::shared_ptr<Renderer const> const &pRenderer);
+
+		[[nodiscard]]
+		std::shared_ptr<Mesh const> const &getMesh() const noexcept;
+		void setMesh(std::shared_ptr<Mesh const> const &pMesh);
+
+		[[nodiscard]]
+		std::shared_ptr<DrawParam const> const &getDrawParam() const noexcept;
+		void setDrawParam(std::shared_ptr<DrawParam const> const &pDrawParam);
+
+		[[nodiscard]]
+		constexpr MaterialPack &getMaterialPack(
+			uint32_t instanceIndex) noexcept;
+
+		[[nodiscard]]
+		constexpr MaterialPack const &getMaterialPack(
+			uint32_t instanceIndex) const noexcept;
+
+		[[nodiscard]]
+		uint32_t getInstanceCount() const noexcept;
+		void setInstanceCount(const uint32_t count);
+
+		[[nodiscard]]
+		constexpr bool isVisible() const noexcept;
+		void setVisible(const bool visible);
+
+		[[nodiscard]]
+		constexpr bool isDrawable() const noexcept;
+
+		void draw(
+			VK::CommandBuffer &cmdBuffer,
+			uint32_t baseId) const noexcept;
+
+		[[nodiscard]]
+		constexpr Infra::EventView<RenderObject const *, Renderer const *, Renderer const *> &
+			getRendererChangeEvent() const noexcept;
+
+		[[nodiscard]]
+		constexpr Infra::EventView<RenderObject const *, Mesh const *, Mesh const *> &
+			getMeshChangeEvent() const noexcept;
+
+		[[nodiscard]]
+		constexpr Infra::EventView<RenderObject const *, DrawParam const *, DrawParam const *> &
+			getDrawParamChangeEvent() const noexcept;
+
+		[[nodiscard]]
+		constexpr Infra::EventView<RenderObject const *, uint32_t, std::type_index, Material const *, Material const *> &
+			getMaterialChangeEvent() const noexcept;
+
+		[[nodiscard]]
+		constexpr Infra::EventView<RenderObject const *, uint32_t, uint32_t> &
+			getInstanceCountChangeEvent() const noexcept;
+
+		[[nodiscard]]
+		constexpr Infra::EventView<RenderObject const *, bool> &
+			getDrawableChangeEvent() const noexcept;
+
+	private:
+		std::shared_ptr<Renderer const> __pRenderer;
+		std::shared_ptr<Mesh const> __pMesh;
+		std::shared_ptr<DrawParam const> __pDrawParam;
+		std::vector<std::unique_ptr<MaterialPack>> __materialPacks;
+
+		bool __visible{ true };
+		bool __drawable{ };
+
+		std::unordered_map<MaterialPack const *, uint32_t> __materialPack2Index;
+
+		mutable Infra::Event<RenderObject const *, Renderer const *, Renderer const *>
+			__rendererChangeEvent;
+
+		mutable Infra::Event<RenderObject const *, Mesh const *, Mesh const *>
+			__meshChangeEvent;
+
+		mutable Infra::Event<RenderObject const *, DrawParam const *, DrawParam const *>
+			__drawParamChangeEvent;
+
+		mutable Infra::Event<RenderObject const *, uint32_t, std::type_index, Material const *, Material const *>
+			__materialChangeEvent;
+
+		mutable Infra::Event<RenderObject const *, uint32_t, uint32_t>
+			__instanceCountChangeEvent;
+
+		mutable Infra::Event<RenderObject const *, bool>
+			__drawableChangeEvent;
+
+		Infra::EventListenerPtr<MaterialPack const *, std::type_index, Material const *, Material const *>
+			__pMaterialPackMaterialChangeListener;
+
+		[[nodiscard]]
+		bool __resolveDrawable() const noexcept;
+		void __validateDrawable();
+
+		void __onMaterialPackMaterialChanged(
+			MaterialPack const *pPack,
+			std::type_index const &type,
+			Material const *pPrev,
+			Material const *pCur);
 	};
+
+	constexpr MaterialPack &RenderObject::getMaterialPack(
+		uint32_t const instanceIndex) noexcept
+	{
+		return *(__materialPacks[instanceIndex]);
+	}
+
+	constexpr MaterialPack const &RenderObject::getMaterialPack(
+		uint32_t const instanceIndex) const noexcept
+	{
+		return *(__materialPacks[instanceIndex]);
+	}
+
+	constexpr bool RenderObject::isVisible() const noexcept
+	{
+		return __visible;
+	}
+
+	constexpr bool RenderObject::isDrawable() const noexcept
+	{
+		return __drawable;
+	}
+
+	constexpr Infra::EventView<RenderObject const *, Renderer const *, Renderer const *> &
+		RenderObject::getRendererChangeEvent() const noexcept
+	{
+		return __rendererChangeEvent;
+	}
+
+	constexpr Infra::EventView<RenderObject const *, Mesh const *, Mesh const *> &
+		RenderObject::getMeshChangeEvent() const noexcept
+	{
+		return __meshChangeEvent;
+	}
+
+	constexpr Infra::EventView<RenderObject const *, DrawParam const *, DrawParam const *> &
+		RenderObject::getDrawParamChangeEvent() const noexcept
+	{
+		return __drawParamChangeEvent;
+	}
+
+	constexpr Infra::EventView<RenderObject const *, uint32_t, std::type_index, Material const *, Material const *> &
+		RenderObject::getMaterialChangeEvent() const noexcept
+	{
+		return __materialChangeEvent;
+	}
+
+	constexpr Infra::EventView<RenderObject const *, uint32_t, uint32_t> &
+		RenderObject::getInstanceCountChangeEvent() const noexcept
+	{
+		return __instanceCountChangeEvent;
+	}
+
+	constexpr Infra::EventView<RenderObject const *, bool> &
+		RenderObject::getDrawableChangeEvent() const noexcept
+	{
+		return __drawableChangeEvent;
+	}
 }
